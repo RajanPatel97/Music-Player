d71127998a53a0745d064c67555ade918491643f
RajanPatel97 <rajan.n.patel.97@gmail.com>
2020-08-01 16:48:30 +0100

    added progress updates

--- script.js
+++ script.js
@@ -5,6 +5,10 @@ const music = document.querySelector('audio');
 const prevBtn = document.getElementById('prev');
 const playBtn = document.getElementById('play');
 const nextBtn = document.getElementById('next');
+const progress = document.getElementById('progress');
+const progressContainer = document.getElementById('progress-container');
+const currentTimeEl = document.getElementById('current-time');
+const durationEl = document.getElementById('duration');
 
 // Check if Playing
 let isPlaying = false;
@@ -82,10 +86,48 @@ function loadSong(song) {
     image.src = `img/${song.name}.jpg`;
 }
 
+// Update Progress Bar & Time
+function updateProgressBar(e) {
+    if (isPlaying) {
+      const { duration, currentTime } = e.srcElement;
+      // Update progress bar width
+      const progressPercent = (currentTime / duration) * 100;
+      progress.style.width = `${progressPercent}%`;
+      // Calculate display for duration
+      const durationMinutes = Math.floor(duration / 60);
+      let durationSeconds = Math.floor(duration % 60);
+      if (durationSeconds < 10) {
+        durationSeconds = `0${durationSeconds}`;
+      }
+      // Delay switching duration Element to avoid NaN
+      if (durationSeconds) {
+        durationEl.textContent = `${durationMinutes}:${durationSeconds}`;
+      }
+      // Calculate display for currentTime
+      const currentMinutes = Math.floor(currentTime / 60);
+      let currentSeconds = Math.floor(currentTime % 60);
+      if (currentSeconds < 10) {
+        currentSeconds = `0${currentSeconds}`;
+      }
+      currentTimeEl.textContent = `${currentMinutes}:${currentSeconds}`;
+    }
+}
+
+// Set Progress Bar
+function setProgressBar(e) {
+    const width = this.clientWidth;
+    const clickX = e.offsetX;
+    const { duration } = music;
+    music.currentTime = (clickX / width) * duration;
+}
+
 // On Load - Select First Song
 loadSong(songs[songIndex]);
 
-// Play or Pause Event Listener
+// Event Listeners
 playBtn.addEventListener('click', () => (isPlaying ? pauseSong() : playSong()));
 prevBtn.addEventListener('click', prevSong);
 nextBtn.addEventListener('click', nextSong);
+music.addEventListener('ended', nextSong);
+music.addEventListener('timeupdate', updateProgressBar);
+progressContainer.addEventListener('click', setProgressBar);
